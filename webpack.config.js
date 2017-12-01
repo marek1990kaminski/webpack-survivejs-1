0c0814b2b93878eddc90d8b29510c87b67f5d665
Marek K <marekkaminski26@gmail.com>
2017-12-01 11:59:11 +0100

    very basic configuration, that will create one dummy HTML, and there is no scripts in package.json, so I have to execute it by running 'node_modules\.bin\webpack'

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+    app: path.join(__dirname, "app"),
+    build: path.join(__dirname, "build"),
+};
+
+module.exports = {
+
+    entry: {
+        app: PATHS.app,
+    },
+    output: {
+        path: PATHS.build,
+        filename: "[name].js", // In this case [name] will be replaced by the name of the entry - 'app'.
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Webpack demo",
+        }),
+    ],
+};
